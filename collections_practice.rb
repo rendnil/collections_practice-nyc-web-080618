acee58f291ea25e747d2ff198f98728cd3c2b40f
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-08-06 20:18:41 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,6 +10,6 @@ def sort_array_desc(array)
 end  
 
 def sort_array_char_count(array)
-  #new_array = array.sort_by {|word| word.length}
-  new_array = array.sort{|x, y| x.length <=> y.length}
+  new_array = array.sort_by {|word| word.length}
+  #new_array = array.sort{|x, y| x.length <=> y.length}
 end
